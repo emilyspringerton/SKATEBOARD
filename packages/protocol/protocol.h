8a4deb363a13c3dafa21647b19a621dc38586072
Emily Springerton <emily@farthq.com>
2026-01-30 05:00:11 +0000

    feat: Phase 015 - Implement Network Protocol Layer

--- packages/protocol/protocol.h
+++ packages/protocol/protocol.h
@@ -2,73 +2,45 @@
 #define PROTOCOL_H
 
 #include <stdint.h>
+#include "physics.h"
 
-#define PORT 5314 
-#define MAX_CLIENTS 10
-#define MAX_WEAPONS 5
+#define PROTOCOL_VERSION 485
+#define MAX_CLIENTS 32
+#define PORT 7777
 
-#define PACKET_INPUT 1
-#define PACKET_STATE 2
-#define PACKET_LOBBY_CMD 3
-#define PACKET_LOBBY_INFO 4
+// --- PACKET TYPES ---
+typedef enum {
+    PKT_C2S_INPUT = 1,
+    PKT_S2C_STATE = 2,
+    PKT_C2S_JOIN  = 3
+} PacketType;
 
-#define CMD_SEARCH 1
-#define CMD_CREATE 2
-#define CMD_JOIN   3
-#define CMD_NAME   5
-#define CMD_ENTER_VEHICLE 6
+// --- DATA STRUCTURES ---
+#pragma pack(push, 1)
 
-#define WPN_KNIFE 0
-#define WPN_MAGNUM 1
-#define WPN_AR 2
-#define WPN_SHOTGUN 3
-#define WPN_SNIPER 4
-
-typedef struct { float x, y, z; } Vec3;
-
+// Sent from Client to Server every frame
 typedef struct {
-    int active;
-    Vec3 pos; Vec3 vel;
-    float yaw, pitch, roll;
-    int driver_id;
-} VehicleState;
+    uint8_t type;
+    uint32_t sequence;
+    float fwd;
+    float strafe;
+    float yaw;
+    uint8_t buttons; // Bitmask for Jump, Fire, etc.
+} MsgInput;
 
+// Sent from Server to Client (The Snapshot)
 typedef struct {
-    int active;
-    char name[32];
-    Vec3 pos; Vec3 vel;
-    float yaw; float pitch;
-    int health; int kills;
-    int current_weapon; int ammo[MAX_WEAPONS];
-    int attack_cooldown; int is_shooting;
-    int hit_feedback; int color; int reload_timer;
-    int in_vehicle;
-    float ground_friction; // <-- NEW: Current traction
-} PlayerState;
+    uint8_t type;
+    uint32_t sequence;
+    uint32_t server_time;
+    struct {
+        uint8_t active;
+        Vec3 pos;
+        Vec3 vel;
+        float yaw;
+    } players[MAX_CLIENTS];
+} MsgWorldState;
 
-typedef struct {
-    int server_tick;
-    int player_count;
-    char status_msg[64];
-    PlayerState players[MAX_CLIENTS];
-    VehicleState vehicle;
-} ServerState;
-
-typedef struct {
-    int type; int owner_id; int cmd_id; char data[4096];
-} Packet;
-
-typedef struct {
-    float fwd; float strafe; float yaw; float pitch;
-    int jump; int crouch; int shoot; int reload;
-    int weapon_req; int zoom; int use;
-} ClientInput;
-
-typedef struct { int id; int dmg; int rof; int cnt; float spr; int ammo_max; float range; } WeaponStats;
-static WeaponStats WPN_STATS[MAX_WEAPONS] = {
-    {0, 45, 20, 1, 0.0f, 0, 3.5f}, {1, 24, 12, 1, 0.0f, 12, 200.0f}, 
-    {2, 12, 6, 1, 0.04f, 30, 200.0f}, {3, 10, 50, 12, 0.22f, 8, 70.0f}, {4, 95, 70, 1, 0.0f, 5, 500.0f}
-};
+#pragma pack(pop)
 
 #endif
-
